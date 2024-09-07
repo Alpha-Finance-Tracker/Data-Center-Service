493ab27c5e63648ff01c27b3656b790c4b3b0ee8
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-09-07 11:37:57 +0300

    Update documentation for endpoints and Readme

--- README.md
+++ README.md
@@ -30,3 +30,39 @@ The Finance Tracker API is built to manage financial expenditures and receipt pr
 Build with docker:
   ```bash
     docker build -t auth_app .
+
+
+Endpoints
+1. Register Kaufland Receipt
+POST /Finance_tracker/kaufland_receipt
+
+Description: Uploads a Kaufland receipt image and processes it to register expenditure.
+Request Body:
+date: Date when the receipt was issued.
+image: The receipt image in the form of an uploaded file.
+Authorization: JWT token required in the Authorization header.
+Response: Confirmation of receipt registration.
+2. Add Expenditure
+POST /Finance_tracker/update
+
+Description: Registers an expenditure for the authenticated user.
+Request Body:
+name (str): Name of the expenditure.
+price (float): Amount spent.
+category (str): Category of expenditure.
+expenditure_type (str): Type of expenditure (e.g., "groceries", "bills").
+date (str): Date of the expenditure.
+Authorization: JWT token required in the Authorization header.
+Response: Confirmation of expenditure registration.
+3. View Expenditures
+POST /Finance_tracker/view_expenditures
+
+Description: Retrieves the list of expenditures for the authenticated user.
+Request Body:
+interval (str): The time interval for viewing expenditures (e.g., "monthly", "yearly").
+column_type (Optional[str]): Type of data column to filter (default: "Optional").
+category (Optional[str]): Expenditure category to filter by (default: "Optional").
+Authorization: JWT token required in the Authorization header.
+Response: List of expenditures for the user.
+Authentication
+All endpoints are secured using JWT authentication. Make sure to pass the token in the Authorization header as Bearer <token>.
--- app/api/routers/finance_tracker.py
+++ app/api/routers/finance_tracker.py
@@ -10,12 +10,21 @@ from app.utils.auth_verification_services import verify_token
 finance_tracker = APIRouter(prefix='/Finance_tracker')
 security = HTTPBearer()
 
-
 @finance_tracker.post('/kaufland_receipt')
-async def kaufland_receipt(date,
+async def kaufland_receipt(date: str,
                            image: UploadFile = File(...),
-
                            credentials: HTTPAuthorizationCredentials = Depends(security)):
+    """
+    Uploads a Kaufland receipt, processes it using Tesseract and OpenAI. And register it in the database.
+
+    Args:
+    - date (str): The date of the receipt.
+    - image (UploadFile): The receipt image file.
+    - credentials (HTTPAuthorizationCredentials): JWT credentials for authentication.
+
+    Returns:
+    - JSON: Response confirming the registration of the receipt.
+    """
     await verify_token(credentials.credentials)
     return await Kaufland(date, image).register_receipt()
 
@@ -23,12 +32,43 @@ async def kaufland_receipt(date,
 @finance_tracker.post('/update')
 async def add_expenditure(data: ExpenditureRegistration,
                           credentials: HTTPAuthorizationCredentials = Depends(security)):
+    """
+    Registers an expenditure for the authenticated user.
+
+    The `ExpenditureRegistration` model contains the following fields:
+    - `name` (str): Name of the expenditure.
+    - `price` (float): Amount spent.
+    - `category` (str): Category of expenditure.
+    - `expenditure_type` (str): Type of expenditure (e.g., "groceries", "bills").
+    - `date` (str): Date of the expenditure.
+
+    Args:
+    - data (ExpenditureRegistration): The expenditure details.
+    - credentials (HTTPAuthorizationCredentials): JWT credentials for authentication.
+
+    Returns:
+    - JSON: Response confirming the registration of the expenditure.
+    """
     user_token = await verify_token(credentials.credentials)
     return await Expenditures(data).register(user_token.get('user_id'))
-
 
 @finance_tracker.post('/view_expenditures')
 async def view_expenditures(data: ExpenditureDisplay,
                             credentials: HTTPAuthorizationCredentials = Depends(security)):
+    """
+    Retrieves and displays the user's expenditures.
+
+    The `ExpenditureDisplay` model contains the following fields:
+    - `interval` (str): The time interval for viewing expenditures (e.g., "monthly", "yearly").
+    - `column_type` (Optional[str]): Type of data column to filter (default: "Optional").
+    - `category` (Optional[str]): Expenditure category to filter by (default: "Optional").
+
+    Args:
+    - data (ExpenditureDisplay): The parameters for viewing expenditures.
+    - credentials (HTTPAuthorizationCredentials): JWT credentials for authentication.
+
+    Returns:
+    - JSON: List of expenditures for the authenticated user.
+    """
     await verify_token(credentials.credentials)
     return await Expenditures(data).display()
